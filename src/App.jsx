34f8095629baf75291528d477622cdd4487c7880
ViRaL-PaTeL4126 <viralkpatel412@gmail.com>
2026-09-04 15:31:08 +0530

    Initial robot dashboard commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RobotDashboard from './RobotDashboard';
+
+function App() {
+  return (
+    <div>
+      <RobotDashboard />
+    </div>
+  );
+}
+
+export default App;
